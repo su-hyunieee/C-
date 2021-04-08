fb62d20985c9589cccaace5f41a76814da0b52ce
KIMCOMSOO <72965534+su-hyunieee@users.noreply.github.com>
2021-04-08 23:20:58 +0900

    Add files via upload

--- 7576_토마토.cpp
+++ 7576_토마토.cpp
@@ -3,21 +3,40 @@
 #include <algorithm>
 using namespace std;
 
-int n, m;
+//가 ㅅㅔ
+int m, n;
 int map[1001][1001];
+int isvisited[1001][1001];
+
+queue<pair<int, int> > q;
 
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
 
-queue<pair<int, int> > q;
-
-void bfs()
+int main()
 {
+
+    cin >> m >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> map[i][j];
+            isvisited[i][j] = -1;
+
+            if (map[i][j] == 1)
+            {
+                q.push(make_pair(i, j));
+                isvisited[i][j] = 0;
+            }
+        }
+    }
 
     while (!q.empty())
     {
-        int y = q.front().first;
-        int x = q.front().second;
+        int x = q.front().first;
+        int y = q.front().second;
 
         q.pop();
 
@@ -26,57 +45,39 @@ void bfs()
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-           
-            if (map[ny][nx] == 0)
+            if (nx <= 0 || ny <= 0 || nx > n || ny > m)
+                continue;
+            if (map[nx][ny] == 0 && isvisited[nx][ny] == -1)
             {
-                map[ny][nx] = map[y][x] + 1;
-
-                q.push(make_pair(ny, nx));
+                isvisited[nx][ny] = isvisited[x][y] + 1;
+                q.push(make_pair(nx, ny));
             }
         }
     }
-}
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+
+    int result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            result = max(result, isvisited[i][j]);
+        }
+    }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+
+                if (map[i][j] == 0 && isvisited[i][j] == -1)
+
+                    result = -1;
+            }
+        }
+
+        cout << result;
+
     
-    cin >> m >> n;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> map[i][j];
-
-            if (map[i][j] == 1)
-            {
-                q.push(make_pair(i, j));
-            }
-        }
+    
     }
-
-    bfs();
-
-    int Max = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (map[i][j] == 0)
-            {
-                cout << "-1";
-                return 0;
-            }
-
-            Max = max(Max, map[i][j]);
-        }
-    }
-
-    cout << Max -1;
-
-    return 0;
-}
